0fa8cfd83aa3d1dc8fa67e18f4b0a03213ff381e
Steve King <steve@kwsites.co.uk>
2013-01-20 07:28:18 +0000

    Set cache-control max-age in seconds Throw an error when supplying bad values to Moment.js

--- src/middleware.js
+++ src/middleware.js
@@ -19,24 +19,32 @@
     * app.use(require('cacheable-middleware')( 86400000 )); // cache all responses for one day
     *
     * @example
-    * app.use(require('cacheable-middleware')( 1, 'day' )); // cache all responses for one day
+    * app.use(require('cacheable-middleware')( 1, 'days' )); // cache all responses for one day
     *
     * @example
     * app.use(require('cacheable-middleware')( 6, 'months' )); // cache all responses for 6 months
     *
     * @see http://momentjs.com/docs/#/durations/
     *
+    * If the duration is omitted (or set to any falsy value) then no default cache lifespan will be added to the response,
+    * if any other value is supplied and moment.js is unable to set the value (ie: the durationKey isn't supported) then
+    * an error will be thrown.
+    *
     * @name Cacheable
-    * @param {Number|String|Object} duration
+    * @param {Number|Object} duration
+    * @param {String} [durationKey] for use when supplying a number and string as accepted by Moment.js
     * @function
     */
-   function Cacheable(duration) {
+   function Cacheable(duration, durationKey) {
       if(!duration) {
          return Cacheable.cachedResponse.bind(this, null);
       }
 
       if(!Moment.isDuration(duration)) {
          duration = Moment.duration.apply(Moment, arguments);
+         if(!duration.asMilliseconds()) {
+            throw new Error('Specified duration would result in no cache lifespan. ' + JSON.stringify([].slice.call(arguments, 0)));
+         }
       }
 
       return Cacheable.cachedResponse.bind(this, duration.asMilliseconds());
@@ -66,7 +74,7 @@
 
       this.set('Expires', then.toDate().toUTCString());
       this.set('Last-Modified', (new Date).toUTCString());
-      this.set('Cache-Control', 'max-age=' + duration.asMilliseconds());
+      this.set('Cache-Control', 'max-age=' + Math.floor(duration.asSeconds()));
       return this;
    };
 
